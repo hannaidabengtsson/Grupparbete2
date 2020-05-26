48c1e5225c27c7dc089e6d74e64353a085d03af5
Elin Andersson <elinviktoria1@gmail.com>
2020-05-26 16:18:20 +0200

    hämtat countries från rest countries api

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 var app = new Vue({
     el: '#app', 
     data: {
-        country: 'Countries'
+        countries: [],
+        search: ''
+    },
+
+    computed:{
+        countries: function() {
+        return this.country.filter((x) => {
+            return x.title.match(this.search)
+        });
     }
+    },
+
+    mounted() {
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response => (
+                this.countries = response.data
+                ))
+            .catch(error => (
+                console.log(error)
+            ));
+    }
+
 })
+
