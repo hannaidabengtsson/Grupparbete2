7d6cb350ab16ad55eff3fbfeba239150eff0f283
Hanna Bengtsson <hannabengtsson@Hannas-MacBook-Pro.local>
2020-05-27 17:13:04 +0200

    Validering av input och error

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ var app = new Vue({
 
     methods: {
         searchBtn: function (event) {
+            if (this.searchInput == "" || this.searchInput.length <= 2){
+                alert("Du måste ange ett land med minst 3 bokstäver!")
+                return;                
+            }
             axios
                 .get(`https://restcountries.eu/rest/v2/name/${this.searchInput}`)
                 .then(response => (
                     this.countries = response.data
                 ))
                 .catch(error => (
-                    console.log(error)
+                    alert("Couldn´t find the country you searched for!")
                 ));
             },
     }
